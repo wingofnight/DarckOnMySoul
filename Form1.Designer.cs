cc4c9a05ca0885a5ca17278e7c37d138433d5163
Владимир <51334865+wingofnight@users.noreply.github.com>
2020-09-27 20:18:28 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DarckOnMySoul
+{
+    partial class FormPicture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPicture));
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.Button_Select = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btn_accept = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picture
+            // 
+            this.Picture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Picture.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Picture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Picture.Image = global::DarckOnMySoul.Properties.Resources._12332133;
+            this.Picture.Location = new System.Drawing.Point(12, 12);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(207, 232);
+            this.Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Picture.TabIndex = 0;
+            this.Picture.TabStop = false;
+            // 
+            // Button_Select
+            // 
+            this.Button_Select.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_Select.Location = new System.Drawing.Point(11, 250);
+            this.Button_Select.Name = "Button_Select";
+            this.Button_Select.Size = new System.Drawing.Size(208, 52);
+            this.Button_Select.TabIndex = 1;
+            this.Button_Select.Text = "Выбрать Фото";
+            this.Button_Select.UseVisualStyleBackColor = true;
+            this.Button_Select.Click += new System.EventHandler(this.Button_Select_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // btn_accept
+            // 
+            this.btn_accept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_accept.Location = new System.Drawing.Point(12, 314);
+            this.btn_accept.Name = "btn_accept";
+            this.btn_accept.Size = new System.Drawing.Size(208, 54);
+            this.btn_accept.TabIndex = 4;
+            this.btn_accept.Text = "Завастить";
+            this.btn_accept.UseVisualStyleBackColor = true;
+            this.btn_accept.Visible = false;
+            this.btn_accept.Click += new System.EventHandler(this.btn_accept_Click);
+            // 
+            // FormPicture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DarckOnMySoul.Properties.Resources.maxresdefault;
+            this.ClientSize = new System.Drawing.Size(231, 378);
+            this.Controls.Add(this.btn_accept);
+            this.Controls.Add(this.Button_Select);
+            this.Controls.Add(this.Picture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormPicture";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Завасть фото";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Button Button_Select;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btn_accept;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DarckOnMySoul
+{
+    public partial class FormPicture : Form
+    {
+        public string F_name;
+        Bitmap bmp;
+        public FormPicture()
+        {
+            InitializeComponent();               
+        }
+
+        private void Button_Select_Click(object sender, EventArgs e)
+        {
+            openFileDialog.InitialDirectory = "C:\\Users\\Public\\Pictures";
+            DialogResult result =  openFileDialog.ShowDialog();
+            LoadPicture(openFileDialog.FileName);
+            F_name = openFileDialog.FileName;
+            int size = F_name.Length - 4;
+            F_name = F_name.Remove(size, 4);
+            btn_accept.Visible = true;
+        }
+        public void LoadPicture(string filename)
+        {
+            try
+            {
+                bmp = new Bitmap(Image.FromFile(filename));
+                Picture.Image = bmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);               
+            }
+        }
+
+        private void btn_accept_Click(object sender, EventArgs e)
+        {
+            ChangePicture();            
+            Picture.Image.Save(F_name + "-result.jpg");
+        }
+        public void ChangePicture()
+        {
+            Bitmap res = new Bitmap(bmp);
+            for (int y = 0; y < bmp.Height; y++)
+                for (int x = 0; x < bmp.Width; x++)              
+            {
+                    Color pixel = bmp.GetPixel(x, y);
+                   
+                     pixel = ChangeGrayscale(pixel);
+                    
+                    res.SetPixel(x, y, pixel);
+            }
+            Picture.Image = res;
+        }
+        private Color ChangeGrayscale(Color pixel)
+        {
+            int avg = (pixel.R + pixel.G + pixel.B + 1) / 3;
+            return Color.FromArgb(avg, avg, avg);
+        }
+    }
+}
